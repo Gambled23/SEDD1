89899439b9729c0a1207dac2453dc437c0a2b07c
Cesar <ipog71@gmail.com>
2022-03-01 12:20:20 -0600

    memin

--- Mod2-Act05/Actividad5_Pila dinamica_Cesar_Gonzalez_Giron.cpp
+++ Mod2-Act05/Actividad5_Pila dinamica_Cesar_Gonzalez_Giron.cpp
@@ -62,22 +62,7 @@ void display(Nodo *&pila, int cont)
     }
 };
 
-int menu()
-{
 
-    int opc;
-
-    cout << "\n  1.- Apilar"
-
-            "\n   2.- Desapilar"
-
-            "\n   0.- Salir"
-         << endl;
-
-    cin >> opc;
-
-    return opc;
-};
 
 int main()
 {
@@ -86,45 +71,23 @@ int main()
 
     int value, cont = 0, opc = 1;
 
-    do
-    {
-
-        switch (menu())
-        {
-
-        case 1:
-
-            cont++;
-
-            push(pila, cont);
-
-            system("CLS");
-
-            display(pila, cont);
-
-            break;
-
-        case 2:
-
-            cont--;
-
-            pop(pila);
-
-            system("CLS");
-
-            display(pila, cont);
-
-            break;
-
-        case 0:
-
-            opc = 0;
-
-            break;
-
-        default:
-            std::cout << "\nOpcion no disponible" << '\n';
-        }
+    
 
     } while (opc != 0);
+};
+int menu()
+{
+    int opc;
+    char opcChar;
+    do
+    {
+        /* code */
+    } while (opc != 5);
+    
+    cout << "1) Push" << endl
+         << "2) Pop" << endl
+         << "3) Mostrar tope" << endl
+         << "4) Mostrar pila" << endl
+         << "5) Salir" << endl;
+    return opc;
 };
