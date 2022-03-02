b415603e28e2b298d6a160c431dda6767b48c77b
Cesar <ipog71@gmail.com>
2022-03-02 08:52:05 -0600

    espacios

--- Mod2-Act05/Actividad5_Pila dinamica_Cesar_Gonzalez_Giron.cpp
+++ Mod2-Act05/Actividad5_Pila dinamica_Cesar_Gonzalez_Giron.cpp
@@ -1,93 +1,88 @@
 #include <iostream>
-
 #include <cstdlib>
-
+#include "validaciones.hpp"
 using namespace std;
-
+void menu();
 class Nodo
 {
 
 public:
     int value;
-
     Nodo *next;
 };
 
 void push(Nodo *&pila, int n)
 {
-
     Nodo *temp = new Nodo();
-
     temp->value = n;
-
     temp->next = pila;
-
     pila = temp;
 };
 
 void pop(Nodo *&pila)
 {
-
     Nodo *temp = pila;
-
     pila = temp->next;
-
     delete (temp);
 };
 
 void display(Nodo *&pila, int cont)
 {
-
     Nodo *temp = pila;
-
     while (temp != NULL)
     {
-
         if (temp == pila)
         {
-
             cout << "\t*" << temp->value << "*";
-
             temp = temp->next;
         }
         else
         {
-
             cout << "\t*" << temp->value << "*";
-
             temp = temp->next;
         }
-
         cout << endl;
     }
 };
 
-
-
 int main()
 {
-
     Nodo *pila = NULL;
-
     int value, cont = 0, opc = 1;
-
-    
-
-    } while (opc != 0);
 };
-int menu()
+void menu()
 {
     int opc;
-    char opcChar;
+    char opcChar[100];
     do
     {
-        /* code */
+        cout << "1) Push" << endl
+             << "2) Pop" << endl
+             << "3) Mostrar tope" << endl
+             << "4) Mostrar pila" << endl
+             << "5) Salir" << endl;
+        cin >> opcChar;
+        opc = validarNumInt(opcChar);
+        switch (opc)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            cout << "Gracias por su preferencia" << endl;
+            break;
+        default:
+            cout << "Opcion no valida" << endl;
+            break;
+        }
     } while (opc != 5);
-    
-    cout << "1) Push" << endl
-         << "2) Pop" << endl
-         << "3) Mostrar tope" << endl
-         << "4) Mostrar pila" << endl
-         << "5) Salir" << endl;
-    return opc;
 };
