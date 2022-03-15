8c8d6dae259ffea9e589ecd858b2d245c285ee7d
Cesar <ipog71@gmail.com>
2022-03-15 09:46:13 -0600

    Voy a arreglar la puta mierda del profe

---
+++ Mod3-Act01/agenda/agendabienhecha.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdlib>
+#include "nodo.hpp"
+using namespace std;
+
+void menu();
+int main()
+{
+    menu();
+    system("PAUSE");
+    return 0;
+}
+
+void menu()
+{
+    int opc;
+    do
+    {
+        system("cls");
+        cout << "Menu" << endl;
+        cout << "1) Agregar contacto" << endl;
+        cout << "2) Buscar contacto" << endl;
+        cout << "3) Eliminar contacto" << endl;
+        cout << "4) Buscar por posicion" << endl;
+        cout << "5) Invertir" << endl;
+        cout << "6) Buscar por nombre" << endl;
+        cout << "7) Ordenar alfabeticamente" << endl;
+        cout << "8) Imprimir datos" << endl;
+        cout << "9) Modificar" << endl;
+        cout << "10) Eliminar todo" << endl;
+        cout << "0) Salir" << endl;
+        cin >> opc;
+
+        switch (opc)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            /* code */
+            break;
+        case 6:
+            /* code */
+            break;
+        case 7:
+            /* code */
+            break;
+        case 0:
+            cout << "Gracias por su preferencia" << endl;
+            break;
+        default:
+            cout << "Opcion invalida" << endl;
+            break;
+        }
+        system("PAUSE");
+    } while (opc != 0);
+}
